c0afce2db0f69f08e2e343d4c6f51bcd91af7daa
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-08 14:01:47 -0400

    updated script

--- upwind.py
+++ upwind.py
@@ -11,7 +11,7 @@ class Simulation:
         x_space: np.array = np.arange(-5, 5, 0.01),
         t_space: np.array = np.arange(0, 120, 0.1),
         rho_max: float = 160,
-        v_max: float = 120,
+        v_max: float = 120 / 3600,
     ) -> None:
         self.x_space = x_space
         self.t_space = t_space
@@ -50,27 +50,31 @@ class Simulation:
         ax.set_xlabel("Space")
         ax.set_ylabel("Time")
 
-    def plot_density_animation(self, sol, fig_ax=None):
+    def plot_density_animation(self, sol, fig_ax=None, log_t=False):
         if fig_ax is None:
             fig_ax = plt.subplots()
         fig, ax = fig_ax
 
-        (ln,) = ax.plot(self.x_space, sol[:, 0])
+        (ln,) = ax.plot(self.x_space, sol[:, 0], label="rho")
+        (ln2,) = ax.plot(self.x_space, self.compute_f(sol[:, 0]), label="f")
+        ax.legend()
 
         def init():
             ax.set_xlim(self.x_space[0], self.x_space[-1])
             ax.set_ylim(-20, self.rho_max + 20)
-            ax.set_title("Density vs x")
+            ax.set_title("Density / Flow vs x")
             ax.set_xlabel("x")
-            ax.set_ylabel("rho")
-            return (ln,)
+            ax.set_ylabel("rho / f")
+            return (ln, ln2)
 
         def update(frame_t):
             ln.set_ydata(sol[:, frame_t])
-            print(f"t={np.sum(self.h[:frame_t + 1]):.1f} s {' ' * 40}", end="\r")
-            return (ln,)
+            ln2.set_ydata(self.compute_f(sol[:, frame_t]))
+            if log_t:
+                print(f"t={np.sum(self.h[:frame_t + 1]):.1f} s {' ' * 40}", end="\r")
+            return (ln, ln2)
 
-        _ = FuncAnimation(
+        anim = FuncAnimation(
             fig,
             update,
             frames=range(len(self.t_space)),
@@ -79,17 +83,11 @@ class Simulation:
             interval=self.h[0] * 1000,
         )
 
-        plt.show()
-
-    def update_pdf(self, sol, j):
-        rho = sol[:, j - 1]
-        sol[:-1, j] = rho[:-1] - self.v_max * self.h[j - 1] / self.s * (
-            1 - 2 * rho[:-1] / self.rho_max
-        ) * (rho[1:] - rho[:-1])
+        return anim
 
     def update_textbook(self, sol, j):
         rho = sol[:, j - 1]
-        f = self.v_max * rho * (1 - rho / self.rho_max)
+        f = self.compute_f(rho)
         sol[:-1, j] = rho[:-1] - self.h[j - 1] / self.s * (f[1:] - f[:-1])
         # sol[:-1, j] = sol[:-1, j - 1] - self.v_max * self.h[j - 1] / self.s * (
         #     (1 - sol[1:, j - 1] / self.rho_max) * sol[1:, j - 1]
@@ -98,10 +96,13 @@ class Simulation:
 
     def update_lax_friedrichs(self, sol, j):
         rho = sol[:, j - 1]
-        f = self.v_max * rho * (1 - rho / self.rho_max)
-        sol[1:-1, j] = (rho[2:] + rho[:-2]) / 2 - self.h[j - 1] / self.s[1:] * (
-            f[2:] - f[:-2]
-        ) / 2
+        f = self.compute_f(rho)
+        sol[1:-1, j] = (
+            (rho[2:] + rho[:-2]) / 2 
+            - self.h[j - 1] / self.s[1:] * (
+                f[2:] - f[:-2]
+            ) / 2
+        )
 
     def right_circular_boundary_value(self, sol, j):
         sol[-1, j] = sol[0, j]
@@ -109,6 +110,10 @@ class Simulation:
     def circular_boundary_values(self, sol, j):
         sol[-1, j] = sol[1, j]
         sol[0, j] = sol[-2, j]
+    
+    def compute_f(self, rho: np.array) -> np.array:
+        f = self.v_max * rho * (1 - rho / self.rho_max)
+        return f
 
     @staticmethod
     def compute_differences(space: np.array) -> np.array:
@@ -125,14 +130,16 @@ def main():
     init_rho[x < -0.25] = 80
     init_rho[(x >= -0.25) & (x < 0)] = 160
 
-    sol = s.run_simulation(
-        init_rho, s.update_lax_friedrichs, s.circular_boundary_values
-    )
 
-    fig, axes = plt.subplots(1, 2, figsize=(20, 10))
+    fig, axes = plt.subplots(2, 2, figsize=(15, 15))
 
-    s.plot_density_heatmap(sol, fig_ax=(fig, axes[1]))
-    s.plot_density_animation(sol, fig_ax=(fig, axes[0]))
+    sol = s.run_simulation(init_rho, s.update_textbook, s.right_circular_boundary_value)
+    s.plot_density_heatmap(sol, fig_ax=(fig, axes[0][1]))
+    s.plot_density_animation(sol, fig_ax=(fig, axes[0][0]))
+
+    sol = s.run_simulation(init_rho, s.update_lax_friedrichs, s.circular_boundary_values)
+    s.plot_density_heatmap(sol, fig_ax=(fig, axes[1][1]))
+    s.plot_density_animation(sol, fig_ax=(fig, axes[1][0]), log_t=True)
 
     plt.show()
 
